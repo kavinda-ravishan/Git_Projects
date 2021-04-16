98ec9a59a45bff070868b90768f5e7c4a71d2ea4
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-16 23:49:38 +0530

    immutability

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-//Pure functions
-//same arg => same result
+//Immutability
 
-const { random } = require("lodash");
+const person = { name: "kavinda", color: "red", add: { city: "colombo" } };
 
-//not a pure function
-function rand(num) {
-  return num * Math.random();
-}
+const updated1 = Object.assign({}, person, { name: "ravishan", age: 23 });
+updated1.add.city = "trinco";
+console.log(person);
 
-//not a pure function
-function mul(num) {
-  return num * 2;
-}
+const updated2 = { ...person, name: "a", age: 24, add: { ...person.add } };
+updated1.add.city = "colombo";
+
+console.log(person);
+console.log(updated1);
+console.log(updated2);
