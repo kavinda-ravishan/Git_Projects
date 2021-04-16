ce47670d40b96601f9f0f830324d1b077a8f8347
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-16 17:07:23 +0530

    function composition

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
-function sayHello1() {
-  return "hello1";
-}
+//function composition
+const trim = (str) => str.trim();
+const wrapDiv = (str) => `<div>${str}<div>`;
+const toLowerCase = (str) => str.toLowerCase();
 
-const fn1 = sayHello1;
-
-//higher order function take function as arg or return function or both
-
-//higher order function
-function greet1(fnMessage) {
-  console.log(fnMessage());
-}
-
-greet1(fn1);
-
-//higher order function
-function sayHello2() {
-  return function () {
-    return "hello2";
-  };
-}
-
-const fn2 = sayHello2();
-
-//higher order function
-function greet2(fnMessage) {
-  console.log(fnMessage());
-}
-
-greet2(fn2);
+console.log(wrapDiv(trim(toLowerCase("      loL   "))));
