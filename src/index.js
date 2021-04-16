742aff459145212960207c6369788edcf9f4d29a
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-16 17:16:06 +0530

    composing and piping

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import { compose, pipe } from "lodash/fp";
+
 //function composition
 const trim = (str) => str.trim();
 const wrapDiv = (str) => `<div>${str}<div>`;
 const toLowerCase = (str) => str.toLowerCase();
 
 console.log(wrapDiv(trim(toLowerCase("      loL   "))));
+
+//composing and piping
+
+const transform1 = compose(wrapDiv, trim, toLowerCase);
+console.log(transform1("  HeLlo   "));
+
+const transform2 = pipe(toLowerCase, trim, wrapDiv);
+console.log(transform2("  WoRlD   "));
