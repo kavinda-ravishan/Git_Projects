53f3d422b9957c6288d11b8f90a2391d8cd05391
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-16 17:31:05 +0530

    currying

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import { compose, pipe } from "lodash/fp";
 
 //function composition
 const trim = (str) => str.trim();
-const wrapDiv = (str) => `<div>${str}<div>`;
+//const wrap = (type, str) => `<${type}>${str}</${type}>`;
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
-console.log(wrapDiv(trim(toLowerCase("      loL   "))));
-
-//composing and piping
-
-const transform1 = compose(wrapDiv, trim, toLowerCase);
-console.log(transform1("  HeLlo   "));
-
-const transform2 = pipe(toLowerCase, trim, wrapDiv);
-console.log(transform2("  WoRlD   "));
+const transform = pipe(toLowerCase, trim, wrap("span"));
+console.log(transform("  WoRlD   "));
