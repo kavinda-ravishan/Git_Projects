3d6778a801dcb0acb44cf4b05b19cd932de9a35f
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-16 16:57:27 +0530

    higher order function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+function sayHello1() {
+  return "hello1";
+}
+
+const fn1 = sayHello1;
+
+//higher order function take function as arg or return function or both
+
+//higher order function
+function greet1(fnMessage) {
+  console.log(fnMessage());
+}
+
+greet1(fn1);
+
+//higher order function
+function sayHello2() {
+  return function () {
+    return "hello2";
+  };
+}
+
+const fn2 = sayHello2();
+
+//higher order function
+function greet2(fnMessage) {
+  console.log(fnMessage());
+}
+
+greet2(fn2);
