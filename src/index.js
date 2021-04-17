17b9578bd1526b4f8d24f33c1386a8a92b0120c8
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-18 01:04:15 +0530

    actions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import store from "./store";
 //import { BUG_ADDED, BUG_REMOVED } from "./actionTypes";
-import * as actions from "./actionTypes";
+import * as actions from "./actions";
 
 const unSubscribe = store.subscribe(() => {
   console.log("store changed!", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug 1",
-  },
-});
+store.dispatch(actions.bugAdded("bug 1"));
 
 //bugRemoved not going to notify
-unSubscribe();
+//unSubscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(actions.bugRemoved(1));
 
 console.log(store.getState());
