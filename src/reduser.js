2e6c72506e6ff0f945455ce62dcbedf4094e4272
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-18 00:26:03 +0530

    reducer

---
+++ src/reduser.js
@@ -0,0 +1,19 @@
+let lastID = 0;
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastID,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case "bugRemoved":
+      return state.filter((bug) => bug, id !== action.payload.id);
+    default:
+      return state;
+  }
+}
